e9e59d1d86d809b6ed8dbb5c9c9a065cdba40872
emse-p4a-gwu <john.helveston@gmail.com>
2023-05-10 13:20:02 -0400

    empty gh-pages

--- NEWS.md
+++
@@ -1,3 +0,0 @@
-# factorlock 0.0.1
-
-- First version of the package
--- inst/example/examples.R
+++
@@ -1,7 +0,0 @@
-# Install from github
-# remotes::install_github('jhelvy/factorlock')
-
-# Load package
-library(factorlock)
-library(dplyr)
-library(gapminder)
